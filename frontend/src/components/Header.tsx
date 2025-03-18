7fb6ede2df912316d9c85cfee0fb9282213d8533
andmatcha <59753159+andmatcha@users.noreply.github.com>
2025-03-18 09:01:05 +0900

    feat: :sparkles: ログイン画面実装

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="w-full fixed flex justify-center items-center h-16 bg-blue-950">
+      <p className="text-lg font-bold text-white">王将シフト</p>
+    </header>
+  );
+};
+
+export default Header;
