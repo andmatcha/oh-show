54e16f1df080065854c12f23f494334272625864
andmatcha <59753159+andmatcha@users.noreply.github.com>
2025-03-18 15:19:24 +0900

    feat: :sparkles: inputに制約を追加

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -23,6 +23,7 @@ const Login = () => {
     e.preventDefault();
     if (!formData.email.trim() || !formData.password.trim()) {
       setError("メールアドレスとパスワードを入力してください");
+      return;
     }
 
     const sanitizedData: FormData = {
@@ -41,6 +42,7 @@ const Login = () => {
             <input
               name="email"
               type="email"
+              required
               value={formData.email}
               onChange={handleChange}
               className="w-full border border-gray-400 rounded-md p-2"
@@ -51,6 +53,9 @@ const Login = () => {
             <input
               name="password"
               type="password"
+              required
+              minLength={8}
+              maxLength={32}
               value={formData.password}
               onChange={handleChange}
               className="w-full border border-gray-400 rounded-md p-2"
