48121111493f593379b25c9bbe869d399adebb43
andmatcha <59753159+andmatcha@users.noreply.github.com>
2025-03-18 15:38:12 +0900

    fix: :bug: ボタンの表示切り替えミス修正

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     <header className="w-full fixed h-16 bg-blue-950">
       <div className="relative w-full h-full flex justify-center items-center">
         <p className="text-lg font-bold text-white">王将シフト</p>
-        {pathname !== Url.top || pathname !== Url.login ? (
+        {pathname !== Url.top && pathname !== Url.login ? (
           <Link
             href={Url.top}
             className="absolute top-1/2 -translate-y-1/2 right-4 text-white text-sm"
